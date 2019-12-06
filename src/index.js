3515a3f2aedd75c108670b9dd04a1900300ce170
Hari Shankar <harishankar3296@gmail.com>
2019-12-07 01:32:52 +0530

    CRA Template edited

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Routes from './routes';
 
 ReactDOM.render(
       <Routes />
-    , document.getElementById('root'));
+    , document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
