4ee97019bbefbcd413c2e2ba7772dbd3699c8190
Hari Shankar <harishankar3296@gmail.com>
2019-12-07 14:46:57 +0530

    es warnings removed

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import HeaderMenu from "./HeaderMenu";
 import logo from "../../assets/img/logo.jpg";
 import "./Header.css";
