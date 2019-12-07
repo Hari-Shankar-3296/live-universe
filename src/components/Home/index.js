17a595d86ca47ccb18d96d86a5aae616dfcce281
Hari Shankar <harishankar3296@gmail.com>
2019-12-08 05:25:01 +0530

    es lint warnings removed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,40 +1,40 @@
-import React, { useState } from "react";
-import Slider from "react-slick";
-import { freeze } from "../../helpers/easyConst";
-import Tabs from "../../UIComponents/Tabs";
-import TabsPane from "../../UIComponents/Tabs/TabsPane";
+import React from "react";
+// import Slider from "react-slick";
+// import { freeze } from "../../helpers/easyConst";
+// import Tabs from "../../UIComponents/Tabs";
+// import TabsPane from "../../UIComponents/Tabs/TabsPane";
 import "./Home.css";
 
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 1
-};
+// const settings = {
+//   dots: true,
+//   infinite: true,
+//   speed: 500,
+//   slidesToShow: 3,
+//   slidesToScroll: 1
+// };
 
-const empty = " ";
+// const empty = " ";
 
-const childTabs = freeze([
-  {
-    title: "tab1",
-    id: "tab1",
-    disabled: false,
-    headerStyle: { color: "green" } // will over ride all css
-  },
-  {
-    title: "tab2",
-    id: "tab2"
-    // disabled: true
-  }
-]);
+// const childTabs = freeze([
+//   {
+//     title: "tab1",
+//     id: "tab1",
+//     disabled: false,
+//     headerStyle: { color: "green" } // will over ride all css
+//   },
+//   {
+//     title: "tab2",
+//     id: "tab2"
+//     // disabled: true
+//   }
+// ]);
 
 function Home() {
-  const [currentTab, setCurrentTab] = useState("tab1");
-  function onTabChange(id) {
-    console.log(id, "home render");
-    setCurrentTab(id);
-  }
+  // const [currentTab, setCurrentTab] = useState("tab1");
+  // function onTabChange(id) {
+  //   console.log(id, "home render");
+  //   setCurrentTab(id);
+  // }
 
   return (
     <main className="home-wrap" style={{ height: "150vh" }}>
