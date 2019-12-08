3e1399a6c0d383ac06b32281c69bc8065d8cd59b
Hari Shankar <harishankar3296@gmail.com>
2019-12-08 15:16:20 +0530

    Home and home container changes

--- src/containers/HomeContainer/index.js
+++ src/containers/HomeContainer/index.js
@@ -3,7 +3,8 @@ import Home from "../../components/Home";
 import Header from "../../common/Header";
 import Footer from "../../common/Footer";
 
-export default function HomeContainer() {
+export default function HomeContainer(props) {
+  console.log(props, "home-con");
   return (
     <div className="home-con">
       <Header />
